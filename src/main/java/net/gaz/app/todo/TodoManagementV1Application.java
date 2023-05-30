963d8de30a46c8d7113c78758db5ed33208f10c5
gaz-oracle <fco.gaz.crespo@gmail.com>
2023-05-30 13:08:58 -0600

    1er Commit Incial

---
+++ src/main/java/net/gaz/app/todo/TodoManagementV1Application.java
@@ -0,0 +1,13 @@
+package net.gaz.app.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoManagementV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoManagementV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/net/gaz/app/todo/TodoManagementV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.gaz.app.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoManagementV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
